9189a150b979c3d5acd5d34544bec500093bde6e
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-05 15:34:13 +0600

    added range.js

---
+++ range.js
@@ -0,0 +1,13 @@
+// example-01
+/* let num = 1;
+while(num <= 10){   // focus on the condition
+    console.log(num);
+    num++;
+} */
+
+// example-02
+let num = 50;
+while (num <= 100) {
+    console.log(num);
+    num++;
+}
