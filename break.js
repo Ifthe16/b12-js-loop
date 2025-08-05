fcd6175f758ef4e47c5901932fd12f87a1723b19
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-05 22:09:29 +0600

    added break.js and continue.js files

---
+++ break.js
@@ -0,0 +1,26 @@
+for (let i = 0; i < 150; i++) {
+    console.log(i);
+    if (i >= 5) {
+        break;
+    }
+}
+
+console.log('life after break up');
+
+let n = 54;
+while (n > 25) {
+    console.log(n);
+    if (n < 50) {
+        break;
+    }
+    n--;
+}
+
+let i = 54;
+while (i > 25) {
+    if (i < 50) {
+        break;
+    }
+    console.log(i);
+    i--;
+}
